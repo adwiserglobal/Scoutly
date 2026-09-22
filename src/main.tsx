5388556c50fb6f22428c5e98fe46e803b9d15bb7
adwiserglobal <eduardocamposmachadoalv@gmail.com>
2026-09-22 19:03:38 -0300

    perf(site): keep map setup out of public landing bundle

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './map3dSetup';
 import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
